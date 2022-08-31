6787eccba6b883b0927072fb0bd49bb7780fc330
Giacomo <giacomobnv@gmail.com>
2022-08-31 13:49:54 +0200

    Projektdateien hinzufügen.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.WriteLine("Hello, World!");
+int x = 9, z = 6; double y = 8;
+Console.WriteLine(x-- == y);
+Console.WriteLine(x == (int)y);
+Console.WriteLine("{0:F1}", 0, 123);
+Console.WriteLine(z / y);
+Console.WriteLine(x -= 10);
+Console.WriteLine(x-- == y & true);
+Console.WriteLine(!(z == 6) | false);
+Console.WriteLine((x *= -2) == z);
